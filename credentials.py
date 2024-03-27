6f265d011f163558b765a9eeed4d5ab9a96a965a
blumjulia <157556603+blumjulia@users.noreply.github.com>
2024-03-27 07:35:37 +0100

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,12 +1,12 @@
 config = dict(
     database=dict(
-        user='root',
-        password='!Q2w3e4r%T_2023',
-        host='localhost',
-        port=3306,
-        database='zechlin'
+        user='',
+        password='',
+        host='',
+        port=,
+        database=''
     ),
     access=dict(
-        password='MasterarbeitBlum2024'
+        password=''
     )
 )
